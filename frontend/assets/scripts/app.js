4ab8f5a95a0712831de2e4fc60f875505d44f174
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-03-28 18:31:26 -0400

    Add ability to log in

--- frontend/assets/scripts/app.js
+++ frontend/assets/scripts/app.js
@@ -9,6 +9,7 @@ const statusMessageTemplate = document.getElementById(
 );
 const messageTemplate = document.getElementById('message-template');
 const webSocket = new WebSocket(serverUrl);
+let chosenUsername = '';
 
 messageSendButton.addEventListener('click', () => {
   const message = messageInput.value;
@@ -16,7 +17,8 @@ messageSendButton.addEventListener('click', () => {
   sendMessage(message);
 });
 usernameSubmitButton.addEventListener('click', () => {
-  document.getElementById('login').remove();
+  const username = document.getElementById('username-input').value;
+  sendLogInEvent(username);
 });
 webSocket.addEventListener('open', () => {
   console.log('WebSocket connected');
@@ -27,8 +29,38 @@ webSocket.addEventListener('close', () => {
 webSocket.addEventListener('error', (event) => {
   console.log('WebSocket error:', event);
 });
-webSocket.addEventListener('message', () => {});
+webSocket.addEventListener('message', (event) => {
+  const eventMessage = JSON.parse(event.data);
+  if (eventMessage.eventName === 'error') {
+    showError(eventMessage.content);
+  } else if (
+    eventMessage.eventName === 'log_in' &&
+    eventMessage.user === chosenUsername
+  ) {
+    document.getElementById('login').remove();
+  }
+});
+
+function sendLogInEvent(username) {
+  chosenUsername = username;
+  const event = {
+    eventName: 'log_in',
+    content: username,
+  };
+  webSocket.send(JSON.stringify(event));
+}
 
 function sendMessage(message) {
   console.log('Sending message:', message);
+}
+
+function showError(errorMessage) {
+  const errorTemplate = document.getElementById('error-template');
+  const errorElement = document.importNode(errorTemplate.content, true);
+  errorElement.querySelector('p').textContent = errorMessage;
+  document.body.appendChild(errorElement);
+  setTimeout(() => {
+    const errorElement = document.querySelector('.error');
+    errorElement.remove();
+  }, 3000);
 }
